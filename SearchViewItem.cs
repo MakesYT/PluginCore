a85b222033f1176545ae10b371290205c1ea920d
MakesYT <42534870+MakesYT@users.noreply.github.com>
2023-10-12 23:10:43 +0800

    优化 修改插件加载逻辑将在加载时生成所有数据 优化 便签窗口样式 新增 插件接入搜索框能力

---
+++ SearchViewItem.cs
@@ -0,0 +1,137 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+#endregion
+
+namespace Core.SDKs;
+
+public class SearchViewItem : ICloneable, IDisposable
+{
+    public string? FileName
+    {
+        set;
+        get;
+    }
+
+    public bool? IsVisible
+    {
+        set;
+        get;
+    }
+
+    public bool IsStared
+    {
+        set;
+        get;
+    }
+
+    public bool IsPined
+    {
+        set;
+        get;
+    } = false;
+
+    public HashSet<string>? Keys
+    {
+        set;
+        get;
+    }
+
+    public FileType FileType
+    {
+        set;
+        get;
+    }
+
+    public FileInfo? FileInfo
+    {
+        set;
+        get;
+    }
+
+    public DirectoryInfo? DirectoryInfo
+    {
+        set;
+        get;
+    }
+
+    public string OnlyKey
+    {
+        set;
+        get;
+    } = "";
+
+    public string? Url
+    {
+        set;
+        get;
+    }
+
+    public int IconSymbol
+    {
+        set;
+        get;
+    }
+
+    public Icon? Icon
+    {
+        set;
+        get;
+    }
+
+    public Action<SearchViewItem>? Action
+    {
+        set;
+        get;
+    }
+
+    public object Clone() =>
+        new SearchViewItem()
+        {
+            FileName = FileName,
+            IsVisible = IsVisible,
+            FileType = FileType,
+            IsStared = IsStared,
+            Action = Action,
+            OnlyKey = OnlyKey,
+            FileInfo = FileInfo != null ? new FileInfo(FileInfo.FullName) : null,
+            Icon = (Icon?)Icon?.Clone(),
+            DirectoryInfo = DirectoryInfo != null ? new DirectoryInfo(DirectoryInfo.FullName) : null
+        };
+
+    public void Dispose()
+    {
+        if (Icon != null)
+        {
+            Icon.Dispose();
+            Icon = null;
+        }
+
+        GC.SuppressFinalize(this);
+    }
+}
+
+public enum FileType
+{
+    应用程序,
+    URL,
+    Word文档,
+    PPT文档,
+    Excel文档,
+    PDF文档,
+    图像,
+    剪贴板图像,
+    文件夹,
+    文件,
+    命令,
+    数学运算,
+    UWP应用,
+    自定义情景,
+    便签,
+    自定义,
+    None
+}
