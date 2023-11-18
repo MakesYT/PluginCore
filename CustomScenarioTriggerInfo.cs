da78f3de4f2d404c0a70d5f3ee1039238755b765
MakesYT <42534870+MakesYT@users.noreply.github.com>
2023-11-18 23:25:32 +0800

    优化 界面文字显示 新增 触发器描述

---
+++ CustomScenarioTriggerInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace PluginCore;
+
+public struct CustomScenarioTriggerInfo
+{
+    public string Name
+    {
+        get;
+        set;
+    }
+
+    public string? Description
+    {
+        get;
+        set;
+    }
+}
