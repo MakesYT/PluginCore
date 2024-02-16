c5c014a06c0ce9672b10082a0875231e3611b9b6
MakesYT <42534870+MakesYT@users.noreply.github.com>
2024-02-16 20:18:55 +0800

    1

--- ISearchItemTool.cs
+++ ISearchItemTool.cs
@@ -2,6 +2,19 @@
 
 public interface ISearchItemTool
 {
-    void OpenSearchItem(SearchViewItem? searchViewItem);
+    void OpenFile(SearchViewItem? item);
+
+    void IgnoreItem(SearchViewItem? item);
+
+    void OpenFolder(SearchViewItem? item);
+
+    void RunAsAdmin(SearchViewItem? item);
+
+    void Star(SearchViewItem? item);
+
+    void Pin(SearchViewItem? item);
+
+    void OpenFolderInTerminal(SearchViewItem? item);
+
     void OpenSearchItemByOnlyKey(string onlyKey);
 }
