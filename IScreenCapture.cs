6aabf1b87e2537e8238b59d211997b3d8f6189be
MakesYT <42534870+MakesYT@users.noreply.github.com>
2024-05-19 01:50:29 +0800

    新增 支持用户直接通过截图框选位置

---
+++ IScreenCapture.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Avalonia.Media.Imaging;
+
+namespace PluginCore;
+
+public struct ScreenCaptureInfo()
+{
+    public int Index = 0;
+    public int X = -1;
+    public int Y = -1;
+    public int Width = -1;
+    public int Height = -1;
+}
+
+public interface IScreenCapture
+{
+    public (Queue<Bitmap>, Queue<Bitmap>) CaptureAllScreen();
+
+
+    public (Bitmap?, Bitmap?)? CaptureScreen(ScreenCaptureInfo screenCaptureInfo, bool withMosaic = false);
+
+    public ScreenCaptureInfo GetScreenCaptureInfoByUserManual();
+}
