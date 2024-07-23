3189584289bd1b12e0ff3d41001e5e7c71a63b1c
MakesYT <2696703792@qq.com>
2024-07-23 15:49:34 +0800

    重构 快捷键系统

--- Attribute/ConfigField/ConfigField.cs
+++ Attribute/ConfigField/ConfigField.cs
@@ -10,19 +10,19 @@ namespace PluginCore.Attribute;
 [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
 public class ConfigField<TEnum> : ConfigField where TEnum : struct, Enum
 {
-    public ConfigField(string title, string description, int symbol = 0 ) : 
+    public ConfigField(string title, string description, int symbol = 0) :
         base(title, description, symbol, ConfigFieldType.自定义选项, Enum.GetValues(typeof(TEnum)).Cast<object>().ToArray())
     {
-        
     }
-    
 }
+
 [AttributeUsage(AttributeTargets.Field, AllowMultiple = true, Inherited = true)]
 public class ConfigField : System.Attribute
 {
     public ConfigField(string title, string description, int symbol = 0,
-        ConfigFieldType fieldType = ConfigFieldType.字符串, object[]? options = null  , int maxValue = 0, int minValue = 0, int step = 0
-         )
+        ConfigFieldType fieldType = ConfigFieldType.字符串, object[]? options = null, int maxValue = 0, int minValue = 0,
+        int step = 0
+    )
     {
         Tittle = title;
         Description = description;
@@ -33,49 +33,16 @@ public class ConfigField : System.Attribute
         MinValue = minValue;
         Step = step;
     }
-    
-    public string Tittle
-    {
-        get;
-        set;
-    }
 
-    public string Description
-    {
-        get;
-        set;
-    }
-    
-    public int Symbol
-    {
-        get;
-        set;
-    }
-    public ConfigFieldType FieldType
-    {
-        get;
-        set;
-    }
-    
-    public object[]? Options
-    {
-        get;
-        set;
-    }
-    public int MaxValue
-    {
-        get;
-        set;
-    }
-    public int MinValue
-    {
-        get;
-        set;
-    }
-    public int Step
-    {
-        get;
-        set;
-    }
-   
+    public string Tittle { get; set; }
+
+    public string Description { get; set; }
+
+    public int Symbol { get; set; }
+    public ConfigFieldType FieldType { get; set; }
+
+    public object[]? Options { get; set; }
+    public int MaxValue { get; set; }
+    public int MinValue { get; set; }
+    public int Step { get; set; }
 }
