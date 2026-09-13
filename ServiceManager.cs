1431ad04104476d56059f09cba22e26a0b6a2c27
Maklith <2696703792@qq.com>
2026-09-13 13:53:25 +0800

    *更新版本号至 0.3.5.1

--- ServiceManager.cs
+++ ServiceManager.cs
@@ -8,5 +8,5 @@ public static class ServiceManager
 {
     public static IServiceProvider Services { get; set; }
     
-    public static string Version = "0.3.5.0";
+    public static string Version = "0.3.5.1";
 }
